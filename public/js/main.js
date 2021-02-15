6b36ecdf986ed5e98765e2a2f66a8d4077f306fb
umar rahim <umarmusavir@gmail.com>
2021-02-15 19:37:36 +0530

    hosting

--- public/js/main.js
+++ public/js/main.js
@@ -18,7 +18,7 @@ const getInfo = async(event) =>{
 
     }else{
         try{
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`;
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=8f77c337a57054b5aa69d0d521e08c97`;
             const response = await fetch(url);
             const data = await response.json();
            const arrData = [data];
